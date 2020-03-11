670166e2d22bc61ef50a1c4f0e66f127bba0efa9
rjensen <wino@jentronics.com>
2020-03-10 23:51:03 -0600

    This is a modified tutorial for building a blog. We need to tear it apart for the movie recommendation engine...

---
+++ app/main.py
@@ -0,0 +1,96 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+#from app.auth import login_required
+from app.db import get_db
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    movies = db.execute(
+ 	 'SELECT * FROM movies ORDER BY RANDOM() LIMIT 10;'
+    ).fetchall()
+    return render_template('main/index.html', movies=movies)
+
+@bp.route('/create', methods=('GET', 'POST'))
+## @login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO posts (title, body, user_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['user_id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.post_id, title, body, created, p.user_id, username'
+        ' FROM posts p JOIN users u ON p.user_id = u.user_id'
+        ' WHERE p.post_id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['user_id'] != g.user['user_id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+##@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE posts SET title = ?, body = ?'
+                ' WHERE post_id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('main/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+#@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM posts WHERE post_id = ?', (id,))
+    db.commit()
+    return redirect(url_for('main.index'))
+
+
---
+++ startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ export FLASK_ENV=development
+ export FLASK_APP=app
+ export LC_ALL=C.UTF-8
+ export LANG=C.UTF-8
+ flask run
+

879a43b6c2897e91307b11f59f5c350c0cf49af8
Ron Jensen <ron@jentronics.com>
2020-03-10 21:05:37 -0600

    don't track database files or csv files

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/recommender")
+def recommender():
+    return render_template('layout.html')
+
 if __name__ == "__main__":
     app.run()
